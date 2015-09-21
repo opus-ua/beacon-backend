a40f84297d7e494fd737d9b7bd6489fc3c80974b
Richard Belleville <j.belleville.richard@gmail.com>
2015-09-21 00:58:56 -0500

    Potential fix to problem with /version in CI.

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 
 export GOPATH=`pwd`
 
-if [ -v "${VERSION_NUMBER}" ]; then
+if [ "$#" == 1 ]; then
+    VERSION_NUMBER="$1"
+else
     VERSION_NUMBER=0.0.0
 fi
 
+echo $VERSION_NUMBER
+
 go install -v -ldflags "-X main.version=$VERSION_NUMBER" github.com/opus-ua/beacon-backend
-go test -ldflags "-X main.version=$VERSION_NUMBER" github.com/opus-ua/beacon-backend -v
+go test github.com/opus-ua/beacon-backend -v
